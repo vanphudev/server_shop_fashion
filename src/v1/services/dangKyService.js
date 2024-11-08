d890a697afae2e6926f2a1cb6f637f5266966504
DuyCorn <ngohoangduy0103@gmail.com>
2024-11-09 00:30:51 +0700

    add something

---
+++ src/v1/services/dangKyService.js
@@ -0,0 +1,12 @@
+"use strict";
+const { request } = require("express");
+const __RESPONSE = require("../core");
+const db = require("../models");
+
+const createNewAccount = async (req, res) => {
+   
+};
+
+module.exports = {
+    createNewAccount,
+};
